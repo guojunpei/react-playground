cfcb653f27cf82f4cd7cfe9e6df00c0551653870
guojunpei <gjp599@163.com>
2021-10-22 23:32:05 +0800

    practice render props

--- src/react-tutorial/practice.jsx
+++ src/react-tutorial/practice.jsx
@@ -961,3 +961,64 @@ ReactDOM.render(
   <FilterableProductTable products={PRODUCTS} />,
   document.getElementById('container')
 );
+
+
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+        <h1>移动鼠标!</h1>
+        <p>当前的鼠标位置是 ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+
+        {/*todo*/}
+        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>移动鼠标!</h1>
+        <Mouse />
+      </>
+    );
+  }
+}
