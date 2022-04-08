3557e0ab4c6f44ab9c24db998572c83f4fc3de11
guojunpei <gjp599@163.com>
2022-04-09 00:46:32 +0800

    fix the operation display

--- src/calculator/v2/calculator-input.tsx
+++ src/calculator/v2/calculator-input.tsx
@@ -4,18 +4,23 @@ import { ClickEventType, OperationType } from './types/calculator-btn-props';
 import { getCalculatorBtns } from './utils';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export class CalculatorInput extends React.Component<ClickEventType,OperationType, {}> {
+export class CalculatorInput extends React.Component<
+  ClickEventType,
+  OperationType,
+  {}
+> {
   render() {
-    const { clickEvent, operation} = this.props;
+    const { clickEvent, operation } = this.props;
     return (
       <div className="input">
         {getCalculatorBtns().map((b) => (
           <CalculatorBtn
             key={b.name}
             name={b.name}
-            classNames={`${b.classNames}${operation === b.classNames ? "operation-active" : ""}`}
+            classNames={`${b.classNames}${
+              operation === b.classNames ? 'operation-active' : ''
+            }`}
             clickEvent={clickEvent}
-
           />
         ))}
       </div>
