900b97b82475df254b4d3f8fca29bc06ea5367f6
guojunpei <gjp599@163.com>
2022-05-14 00:11:15 +0800

    add some content

--- src/joyingsoft/home/home2.tsx
+++ src/joyingsoft/home/home2.tsx
@@ -35,10 +35,26 @@ export const Home2: FC = () => {
       <div id='services'>
         <div className='title'>服务</div>
         <div className='container'>
-          <div>云方案</div>
-          <div>跨平台</div>
-          <div>新技术</div>
-          <div>工匠心</div>
+          <div className='content'>
+            <div>png</div>
+            <div>云方案</div>
+            <div>云解决方案，高可靠性，可访问性，和安全性。</div>
+          </div>
+          <div className='content'>
+            <div>png</div>
+            <div>跨平台</div>
+            <div>跨平台多种设备支持，服务随时随地可用。</div>
+          </div>
+          <div className='content'>
+            <div>png</div>
+            <div>新技术</div>
+            <div>不断革新，使用最新技术以产生最佳效果。</div>
+          </div>
+          <div className='content'>
+            <div>png</div>
+            <div>工匠心</div>
+            <div>我们乐于寻找解决方案，热爱创造高效易用的软件。</div>
+          </div>
         </div>
       </div>
     </div>
