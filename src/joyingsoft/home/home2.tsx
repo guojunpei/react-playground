88ddeb73f3e9ec812ddd2f003964609882a008f3
guojunpei <gjp599@163.com>
2022-05-15 23:13:57 +0800

    add font-size and other

--- src/joyingsoft/home/home2.tsx
+++ src/joyingsoft/home/home2.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
-import { appRouterPaths } from '../app-constants';
+// import { Link } from 'react-router-dom';
+// import { appRouterPaths } from '../app-constants';
 import './home2.scss';
 
 export const Home2: FC = () => {
@@ -30,27 +30,29 @@ export const Home2: FC = () => {
       </div>
       <nav />
       <div className="js-banner">
-        <div className="js-banner-content">河南乐软科技有限公司</div>
+        <div className="js-banner-content title-font-size">
+          河南乐软科技有限公司
+        </div>
       </div>
-      <div id='services'>
-        <div className='title'>服务</div>
-        <div className='container'>
-          <div className='content'>
+      <div id="services">
+        <div className="title title-font-size">服务</div>
+        <div className="container">
+          <div className="content">
             <div>png</div>
             <div>云方案</div>
             <div>云解决方案，高可靠性，可访问性，和安全性。</div>
           </div>
-          <div className='content'>
+          <div className="content">
             <div>png</div>
             <div>跨平台</div>
             <div>跨平台多种设备支持，服务随时随地可用。</div>
           </div>
-          <div className='content'>
+          <div className="content">
             <div>png</div>
             <div>新技术</div>
             <div>不断革新，使用最新技术以产生最佳效果。</div>
           </div>
-          <div className='content'>
+          <div className="content">
             <div>png</div>
             <div>工匠心</div>
             <div>我们乐于寻找解决方案，热爱创造高效易用的软件。</div>
